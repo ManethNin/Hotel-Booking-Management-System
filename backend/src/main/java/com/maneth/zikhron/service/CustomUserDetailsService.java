82fbdb68a70a8f3d82b8dab15558630881208010
Maneth Ninduwara <maneth.21@cse.mrt.ac.lk>
2025-07-03 19:59:52 +0530

    build custom service to use spring security to fetch users from database

--- backend/src/main/java/com/maneth/zikhron/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/maneth/zikhron/service/CustomUserDetailsService.java
@@ -1,4 +1,18 @@
 package com.maneth.zikhron.service;
 
-public class CustomUserDetailsService {
+import com.maneth.zikhron.exception.OurException;
+import com.maneth.zikhron.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not Found"));
+    }
 }
