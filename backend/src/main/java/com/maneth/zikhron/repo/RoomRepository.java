c7cc0dfa6672c587ec83f92b6ba16df924f4c4e1
Maneth Ninduwara <maneth.21@cse.mrt.ac.lk>
2025-07-29 23:28:08 +0530

    feat(repo): add custom query methods to RoomRepository

--- backend/src/main/java/com/maneth/zikhron/repo/RoomRepository.java
+++ backend/src/main/java/com/maneth/zikhron/repo/RoomRepository.java
@@ -3,6 +3,14 @@ package com.maneth.zikhron.repo;
 import com.maneth.zikhron.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+    List<String>  findDistinctRoomTypes();
+
+    List<Room> findAvailableRoomsByDatesAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
+    List<Room> getAllAvailableRooms();
 }
