1578a2709a8d1a18d75e1c62fbd126ea853f6a7a
Maneth Ninduwara <maneth.21@cse.mrt.ac.lk>
2025-08-04 01:59:31 +0530

    add homepahe comoponent

---
+++ frontend/zikhron-react/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/common/NavBar';
+import HomePage from './components/home/HomePage';
+import ApiService from './service/ApiService';
+import { ProtectedRoute, AdminRoute } from './service/guard';
+import Footer from './components/common/Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            {/* Public Routes */}
+            <Route exact path="/home" element={<HomePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
