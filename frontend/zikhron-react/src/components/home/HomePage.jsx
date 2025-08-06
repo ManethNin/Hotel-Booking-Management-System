26393e24c0ce9b0210fe162269ee960608d28550
ManethNin <kgmninduwara@gmail.com>
2025-08-06 23:51:25 +0530

    Add room reast, search components to home page

--- frontend/zikhron-react/src/components/home/HomePage.jsx
+++ frontend/zikhron-react/src/components/home/HomePage.jsx
@@ -31,12 +31,12 @@ const HomePage = () => {
             </section>
 
             {/* SEARCH/FIND AVAILABLE ROOM SECTION */}
-            {/* <RoomSearch handleSearchResult={handleSearchResult} />
+            <RoomSearch handleSearchResult={handleSearchResult} />
             <RoomResult roomSearchResults={roomSearchResults} />
 
             <h4><a className="view-rooms-home" href="/rooms">All Rooms</a></h4>
 
-            <h2 className="home-services">Services at <span className="zikhron-color">Zikhron Hotel</span></h2> */}
+            <h2 className="home-services">Services at <span className="zikhron-color">Zikhron Hotel</span></h2>
 
             {/* SERVICES SECTION */}
             {/* <section className="service-section"><div className="service-card">
